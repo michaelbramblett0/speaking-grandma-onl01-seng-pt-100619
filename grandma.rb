a4c9c4ea6fe176fbc91ab75eec76af3ed20652b2
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-10-14 22:29:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-speak_to_grandma = 
+speak_to_grandma = gets.chomp
 if speak_to_grandma == "I Love You Grandma".upcase
   puts "I LOVE YOU TOO PUMPKIN"
 elsif speak_to_grandma = "Hi Nana".upcase
