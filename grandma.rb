d3985cec4b1c6b96299009d8756d5e6163505dc8
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-10-14 23:37:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif string == "HI NANA"
+  elsif string == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
   else 
    "HUH?! SPEAK UP, SONNY!"
