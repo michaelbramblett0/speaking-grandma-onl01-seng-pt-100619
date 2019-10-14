01b4713785fc3385672eb701ae33989c4d7a19aa
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-10-14 23:21:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == "I Love You Grandma".upcase
-  puts "I LOVE YOU TOO PUMPKIN"
-elsif string == "Hi Nana".upcase
-  puts "NO, NOT SINCE 1938!"
-else puts "HUH?! SPEAK UP, SONNY!"
+   puts "I LOVE YOU TOO PUMPKIN"
+  elsif string == "Hi Nana".upcase
+   puts "NO, NOT SINCE 1938!"
+  else 
+   puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
