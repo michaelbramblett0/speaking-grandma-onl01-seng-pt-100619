b77a6f0bca1b7b43093ca62ced375377e535388f
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-10-14 23:36:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif string == "Hi Nana".upcase
+  elsif string == "HI NANA"
     "NO, NOT SINCE 1938!"
   else 
    "HUH?! SPEAK UP, SONNY!"
